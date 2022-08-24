f4ec0dc8720193c8d579808488b05b1abf2b2244
misbah099 <111556255+misbah099@users.noreply.github.com>
2022-08-24 11:30:20 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class pattern {
+	public static void main(String args[])
+	{
+		for(int rows=6;rows>=0;rows--) {
+			for(int col=0;col<=rows;col++) {
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
+	}
+	}
+
