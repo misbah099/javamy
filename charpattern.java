84def0b3a7c48c69a5867329c432097f8b06987a
misbah099 <111556255+misbah099@users.noreply.github.com>
2022-08-24 11:29:25 +0530

    Add files via upload

---
+++ charpattern.java
@@ -0,0 +1,18 @@
+package com.java;
+
+public class charpattern {
+	public static void main(String args[])
+	{
+		int alph=65;
+		
+	for(int i=0;i<=3;i++) {
+		for(int j=0;j<=i;j++) {
+			
+		System.out.print((char)(alph+j));
+		}
+		System.out.println();
+
+	}
+	}
+	}
+
