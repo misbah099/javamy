66425a61778c53cb7a59e0673d892e9858581c67
misbah099 <111556255+misbah099@users.noreply.github.com>
2022-08-24 11:34:18 +0530

    Add files via upload

---
+++ alltable.java
@@ -0,0 +1,20 @@
+package com.java;
+
+public class alltable {
+	public static void main(String args[])
+	{
+	 
+	 int i,m=0,j;
+	 for(i=1;i<=10;i++) {
+		 
+	 
+	 for(j=1;j<=10;j++) {
+		 m=(i*j);
+		 ;
+		 
+	System.out.println(+i+"X"+j+"="+m) ;
+	 }
+	 }
+	}
+	}
+
