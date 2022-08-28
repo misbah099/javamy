30076bfb3fda1459bd6dfe8bc4847dd6738d0711
misbah099 <111556255+misbah099@users.noreply.github.com>
2022-08-28 15:03:38 +0530

    Update Employeemain.java

--- OOPS2NDCLASS/Employeemain.java
+++ OOPS2NDCLASS/Employeemain.java
@@ -1,3 +1,9 @@
+/*Create a class called Employee having the following /atrributes: pan no,Name,Annual taxable income. Calculate the tax of an Employee according to the following criteria
+and display all the information:
+Annual taxable income upto 1,00,000 no tax
+from 1,00,000 to 1,50,000 10% of income exceeding 1,00,000
+from 1,50,001 to 5000+20% of income exceeding 1,50,000
+above 2,50,000  25000+30% of income exceeding 2,50,000 */
 package oopjava.java;
 
 import java.util.Scanner;
