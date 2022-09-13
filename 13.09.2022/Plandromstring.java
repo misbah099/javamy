1cebe12423cc6aecd55a51eb5128d36d8f80962b
misbah099 <111556255+misbah099@users.noreply.github.com>
2022-09-13 12:36:41 +0530

    Add files via upload

---
+++ 13.09.2022/Plandromstring.java
@@ -0,0 +1,25 @@
+/* WAP to input a string and check whether it is palindrome or not */
+package Classfiles.java;
+
+import java.util.Scanner;
+
+public class Plandromstring {
+
+	public static void main(String[] args) {
+		String s1,temp,rev="";
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a String=");
+		s1=sc.nextLine();
+		temp=s1;
+		for(int i=s1.length()-1;i>=0;i--) {
+			rev+=s1.charAt(i);
+		}
+		if(rev.equals(temp)) {
+			System.out.println("Pallindrome String");
+		}
+		else {
+			System.out.println("Not a Pallindrome string");
+		}
+
+}
+}
