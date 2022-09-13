fe6a539d565b1dc9eb0e089d627d3b885e89f730
misbah099 <111556255+misbah099@users.noreply.github.com>
2022-09-13 11:57:11 +0530

    Add files via upload

---
+++ 12.09.2022/DeleteConsonent.java
@@ -0,0 +1,36 @@
+/*  */
+package Classfiles.java;
+
+import java.util.Scanner;
+
+public class DeleteConsonent {
+public static void main(String[] args) {
+	 String s;
+	 int j=0;
+     System.out.println("Enter a string");
+     Scanner so=new Scanner(System.in);
+	 s= so.nextLine();
+ char ch[]=new char[26];
+
+ for(int i=0;i<s.length();i++)
+ {
+ if(s.charAt(i)=='a'|| s.charAt(i)=='A'||s.charAt(i)=='e'|| s.charAt(i)=='E'||s.charAt(i)=='i'|| s.charAt(i)=='I'||s.charAt(i)=='o'|| s.charAt(i)=='O'||s.charAt(i)=='U'|| s.charAt(i)=='u')
+ {
+ ch[j++]=s.charAt(i);
+ }
+ else
+ {	        
+	continue;
+   }
+  }
+for(int i=0;i<j;i++)
+ {
+  System.out.print(ch[i]);
+ }
+  System.out.println();
+	}
+}
+		    
+	
+
+
