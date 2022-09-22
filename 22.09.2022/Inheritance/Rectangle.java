f8b5c29b7dfeb8cf972360bf8111696a6d1ae9d5
misbah099 <111556255+misbah099@users.noreply.github.com>
2022-09-22 12:22:06 +0530

    Update Rectangle.java

--- 22.09.2022/Inheritance/Rectangle.java
+++ 22.09.2022/Inheritance/Rectangle.java
@@ -6,11 +6,4 @@ public void calculateArea() {
 	area=length*breadth;
 	System.out.print("Area of Rectangle is= "+area);
 }
-/*public void length() {
-System.out.println("Enter length of rectangle="+length)	;
-}
-public void breadth() {
-	System.out.println("Enter breadth of rectangle="+breadth)	;
-	
-}*/
 }
